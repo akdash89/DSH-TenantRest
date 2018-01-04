ec9ea42b2d05863df36bbe69a00d101788975ceb
Partapsing, Rakesh <rakesh.partapsing@kpn.com>
2018-01-04 15:20:25 +0100

    adjusted testcases script

--- dockerRF/changeme.sh
+++ dockerRF/changeme.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 IP=`ifconfig | grep inet | grep -v inet6 | head -1 | awk '{print $2}' | cut -d':' -f2`
-sed -i -e "s/IPADDRESS/$IP/g" "makefile"
+#sed -i -e "s/IPADDRESS/$IP/g" "makefile"
+
+: "${pathToFileKafkaRobot:="$PWD/Testcases/kafka/kafka.robot"}"
+
+if [ -f "$pathToFileKafkaRobot" ]; then
+  sed -i -e "s/IPADDRESS/$IP/g" "$pathToFileKafkaRobot"
+fi
