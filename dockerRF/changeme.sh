b0e76b04e0e3fcfa42485187fdc6c7aaecd13e42
Partapsing, Rakesh <rakesh.partapsing@kpn.com>
2018-01-04 17:47:26 +0100

    typo in changeme file

--- dockerRF/changeme.sh
+++ dockerRF/changeme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP=`ifconfig | grep inet | grep -v inet6 | head -1 | awk '{print $2}' | cut -d':' -f2`
-#sed -i -e "s/IPADDRESS/$IP/g" "makefile"
+sed -i -e "s/IPADDRESS/$IP/g" "makefile"
 
 : "${pathToFileKafkaRobot:="$PWD/Testcases/kafka/kafka.robot"}"
 
